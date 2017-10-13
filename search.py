027b447ca33a8100303c36b30fe1eb735161915c
RodrigoGuerra <rodrigoguerra.rj@gmail.com>
2017-10-13 13:06:08 -0300

    etapas 1,2 e 4 prontas

--- search.py
+++ search.py
@@ -72,6 +72,23 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
+def depthSearch(problem, state, visited=[]):
+    visited.append(state)
+    for successor in problem.getSuccessors(state):
+        state = successor[0]
+        direction = successor[1]
+
+        if problem.isGoalState(state):
+            return [direction]
+
+        if state not in visited:
+            search = depthSearch(problem, state, visited)
+            if len(search) > 0:
+                search.insert(0, direction)
+                return search
+
+    return []
+
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
@@ -87,17 +104,76 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return depthSearch(problem, problem.getStartState())
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    queue = util.Queue()
+    queue.push((problem.getStartState(), []))
+    visited = []
+
+    while not queue.isEmpty():
+        current = queue.pop()
+        currentState = current[0]
+        currentPath = current[1]
+
+        visited.append(currentState)
+
+        for successor in problem.getSuccessors(currentState):
+            state = successor[0]
+            direction = successor[1]
+
+            path = list(currentPath)
+            path.append(direction)
+
+            if problem.isGoalState(state):
+                return path
+
+            if state not in visited:
+                queue.push((state, path))
+
+    return []
+
+def graphSearch(problem, allPaths):
+    exploredNodes = []
+    allPaths.push([(problem.getStartState(),"Stop", 0)])
+
+    while not allPaths.isEmpty():
+        #Seleciona um caminho (o do topo da piha)
+        path = allPaths.pop()
+
+        #pega a ultima posicao e salva em lastNodeOfThePath
+        lastNodeOfThePath = path[len(path) - 1]
+
+        print(lastNodeOfThePath)
+
+        lastNodeOfThePath = lastNodeOfThePath[0]
+
+        # Se essa posicao for solucao retorna o caminho ate essa posicao, eliminando a primeira entrada que e a posicao inicial do pacman
+        if problem.isGoalState(lastNodeOfThePath):
+            return [x[1] for x in path][1:]
+
+        # Se a posicao nao foi explorada ainda, adiciona ela na lista de nos explorados e constroi os caminhos possiveis pra cada direcao
+        # a partir dessa posicao, inserindo-os em allPaths para serem processados nas proximas iteracoes.
+        if lastNodeOfThePath not in exploredNodes:
+            exploredNodes.append(lastNodeOfThePath)
+
+            for nextNode in problem.getSuccessors(lastNodeOfThePath):
+                if nextNode[0] not in exploredNodes:
+                    nextNodePath = path[:]
+                    nextNodePath.append(nextNode)
+                    allPaths.push(nextNodePath)
+
+    #retorna [] caso nao encontre a comida por nenhum caminho
+    return []
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    costLambda = lambda aPath: problem.getCostOfActions([x[1] for x in aPath])
+    allPaths = util.PriorityQueueWithFunction(costLambda)
+    return graphSearch(problem, allPaths)
 
 def nullHeuristic(state, problem=None):
     """
@@ -109,7 +185,26 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    nos = [] #conjunto de nos analisados
+    queue = util.PriorityQueue() #queue que retorna item de menor prioridade
+    start = problem.getStartState() #ponto inicial
+    queue.push( (start, []), heuristic(start, problem))
+    while not queue.isEmpty():
+        no, actions = queue.pop() #pega elemento de menor custo
+
+        if problem.isGoalState(no):
+            #retorna acoes necessarias p/ chegar a semente
+            return actions
+
+        nos.append(no)
+
+        for coord, direction, cost in problem.getSuccessors(no):
+            if not coord in nos: #se o no ainda nao foi analisado
+                new_actions = actions + [direction]
+                custo = problem.getCostOfActions(new_actions) + heuristic(coord, problem) #calcula custo das acoes
+                queue.push( (coord, new_actions), custo)
+    return []
+
 
 
 # Abbreviations
